ca8d8e5aaab2499428e8e2972d57ad051674c934
CristianPeter <horatiucristianpeter@gmail.com>
2025-01-30 15:55:32 +0100

    minor fix on NumchekrNode

--- src/Core/Phone/Nodes/NumcheckrAdaptorNode.php
+++ src/Core/Phone/Nodes/NumcheckrAdaptorNode.php
@@ -2,26 +2,29 @@
 
 namespace CristianPeter\LaravelDisposableContactGuard\Core\Phone\Nodes;
 
-use CristianPeter\LaravelDisposableContactGuard\Facades\DisposableNumbers;
+use CristianPeter\LaravelDisposableContactGuard\DisposableNumbers;
 use CristianPeter\LaravelDisposableContactGuard\Integrations\NumCheckR\Exceptions\ApiErrorException;
 use CristianPeter\LaravelDisposableContactGuard\Integrations\NumCheckR\NumCheckrManager;
 use Illuminate\Http\Client\ConnectionException;
+use Psr\SimpleCache\InvalidArgumentException;
 
 class NumcheckrAdaptorNode implements PhoneAdaptorNode
 {
     /**
      * @throws ApiErrorException
      * @throws ConnectionException
+     * @throws InvalidArgumentException
      */
     public function isNotDisposable(mixed $number): bool
     {
         $manager = app(NumCheckrManager::class);
         $isNotDisposable = $manager->isNotDisposable($number);
         if (! $isNotDisposable) {
-            $numberList = DisposableNumbers::getFromCache(DisposableNumbers::$cacheKey);
+            $disposableNumbers = app(DisposableNumbers::class);
+            $numberList = $disposableNumbers->getFromCache($disposableNumbers->getCacheKey());
             if (! isset($numberList[$number])) {
                 $numberList[$number] = $number;
-                DisposableNumbers::saveToStorage($numberList);
+                $disposableNumbers->saveToStorage($numberList);
             }
         }
         return $isNotDisposable;
